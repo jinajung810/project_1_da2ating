80eb497b03f18ba40f91ad0e28171a502495d067
jinajung810 <jjina810@gmail.com>
2023-07-07 02:04:00 +0900

    salad 상품목록 페이지 데이터 연결

--- src/views/product-list/salad/salad.js
+++ src/views/product-list/salad/salad.js
@@ -1,89 +1,37 @@
 
-let saladInfo = [
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드1',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
+fetch('http://127.0.0.1:5555/api/products', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
   },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드2',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드3',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드4',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드5',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드6',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드7',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드8',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-  {
-    imgSrc: '../images/salad-list-item01.jpg',
-    name: '크래미 샐러드9',
-    discountRate: 23,
-    price: '6,900',
-    originalPrice: '9,000',
-  },
-]
+})
+  .then((res)=> res.json())
+  .then((data)=>{
+    const saladInfo = data; 
+    
+    document.querySelector('.list-num').textContent = saladInfo.length; 
 
-for (let i = 0; i < saladInfo.length; i++) {
-  document.getElementById("salad").innerHTML += `
-    <li>
-      <a href="javascript:void(0)">
-        <img src= ${saladInfo[i].imgSrc} alt="" />
-        <h3>${saladInfo[i].name}</h3>
-      </a>
-      <div class="cart">
-        <a href="javascript:void(0)">
-          <img src="../images/cart_icn.png" alt="">
-        </a>
-      </div>
-      <span>${saladInfo[i].discountRate}%</span>
-      <strong>
-        ${saladInfo[i].price}<span>원</span>
-        <b>${saladInfo[i].originalPrice}원</b>
-      </strong>
-    </li>
-  `
-}
+    for (let i = 1; i < saladInfo.length; i++) {
+      // saladInfo[0].name의 글자수가 길어서 CSS의 수정이 필요함...
+      // 어떤 상품을 목록별로 분류할지 정하고서 전반적인 CSS 수정을 해야할 것 같음
+      document.getElementById("salad").innerHTML += `
+        <li>
+          <a href="javascript:void(0)">
+            <img src= '../images/salad-list-item01.jpg' alt="" />
+            <h3>${saladInfo[i].name}</h3>
+          </a>
+          <div class="cart">
+            <a href="javascript:void(0)">
+              <img src="../images/cart_icn.png" alt="">
+            </a>
+          </div>
+          <span>${saladInfo[i].discountRate}%</span>
+          <strong>
+            ${saladInfo[i].originPrice}<span>원</span>
+            <b>${saladInfo[i].originPrice}원</b>
+          </strong>
+        </li>
+      `
+    }
+  })
 
-document.querySelector('.list-num').textContent = saladInfo.length; 
