01b02951cde5939df7ed68ead589a99d91461059
jinajung810 <jjina810@gmail.com>
2023-07-06 16:53:28 +0900

    style: 페이지 구조 작성

---
+++ src/views/cart/cart.js
@@ -0,0 +1,6 @@
+const salad = [
+    { imgSrc: 'https://via.placeholder.com/150', name: "닭가슴살 샐러드 ", price: 18900, quantity: 1 },
+    { imgSrc: 'https://via.placeholder.com/150', name: "단호박 샐러드", price: 15800, quantity: 3 },
+    { imgSrc: 'https://via.placeholder.com/150', name: "리코타치즈 샐러드", price: 19800, quantity: 5 },
+];
+
