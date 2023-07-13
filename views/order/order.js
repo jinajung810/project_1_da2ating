82b42b226b01bb4fe05bd72dcd630b89e8e08390
jinajung810 <jjina810@gmail.com>
2023-07-14 03:21:49 +0900

    feat : 상세페이지->주문 연결

--- views/order/order.js
+++ views/order/order.js
@@ -1,3 +1,5 @@
+const API_BASE_URL = `http://127.0.0.1:5555`;
+
 // 구매자 정보(이름, 번호, 주소, 이메일, 배송 메세지)
 const deliveryName = document.querySelector('#deliveryName');
 const phoneNumber1 = document.querySelector('#phoneNumber1');
@@ -8,26 +10,13 @@ const addressBasic = document.querySelector('#addressBasic');
 const addressDetail = document.querySelector('#addressDetail');
 const deliveryMessage = document.querySelector('#deliveryMessage');
 
-//구매 상품 임시 데이터
-const productData = [
-  {
-    productId: 1,
-    img: 'https://atowertr6856.cdn-nhncommerce.com/data/goods/20/10/43/1000000060/1000000060_list_075.jpg',
-    productName: '닭가슴살 비엔나 샐러드',
-    amount: 1,
-    price: 9000,
-  },
-  {
-    productId: 2,
-    img: 'https://atowertr6856.cdn-nhncommerce.com/data/goods/16/07/14/72/72_list_032.jpg',
-    productName: '불고기 샐러드',
-    amount: 1,
-    price: 8000,
-  },
-];
+//구매 상품 데이터(localStorage)
+const productData = JSON.parse(localStorage.getItem('buyProduct'));
 
 // 구매자 정보 확인하기
 const onCheckInfo = () => {
+  const regex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+
   if (!deliveryName.value) {
     alert('받으시는 분 이름을 입력해주세요.');
   } else if (
@@ -37,9 +26,9 @@ const onCheckInfo = () => {
   ) {
     alert('휴대폰 번호를 입력해주세요.');
   } else if (
-    phoneNumber1.value > 5 ||
-    phoneNumber2.value > 5 ||
-    phoneNumber3.value > 5
+    !regex.test(
+      `${phoneNumber1.value}-${phoneNumber2.value}-${phoneNumber3.value}`
+    )
   ) {
     alert('올바른 휴대폰 번호를 입력해주세요.');
   } else if (
@@ -59,7 +48,7 @@ const onClickPurchaseBtn = async (e) => {
 
   // 맞게 입력 했을 경우 주문 post
   if (onCheckInfo()) {
-    // 필요한 상품 정보만 추출(임시 데이터)
+    // 필요한 상품 정보만 추출
     const products = productData.reduce((list, product) => {
       list.push({
         id: product.productId,
@@ -70,31 +59,31 @@ const onClickPurchaseBtn = async (e) => {
     }, []);
 
     const orderData = {
-      // 임시 상품 데이터
       products: products,
-      receiver: {
-        name: deliveryName.value,
-        phone: phoneNumber1.value + phoneNumber2.value + phoneNumber3.value,
-        address: addressBasic.value + addressDetail.value,
-      },
-      deliveryMessage: deliveryMessage.value,
+      receiverName: deliveryName.value,
+      receiverPhone: `${phoneNumber1.value}-${phoneNumber2.value}-${phoneNumber3.value}`,
+      receiverAddress: addressBasic.value + addressDetail.value,
+      deliveryMessage:
+        deliveryMessage.value === '' ? undefined : deliveryMessage.value,
     };
 
     const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: '토큰',
+        Authorization: sessionStorage.getItem('token'),
       },
       body: JSON.stringify(orderData),
     };
 
     try {
-      const res = await fetch('http://127.0.0.1:5555/api/orders', options);
+      const res = await fetch(`${API_BASE_URL}/api/orders`, options);
+      console.log(res, orderData);
 
       // 성공 시 페이지 이동
       if (res.ok) {
-        //주문완료 페이지로 이동?
+        // 주문 완료
+        window.location.href = '/views/order/order-complete.html';
       } else {
         alert('다시 시도해주세요!');
       }
@@ -115,7 +104,7 @@ purchaseButton.addEventListener('click', onClickPurchaseBtn);
 const onSearchAddress = () => {
   new daum.Postcode({
     oncomplete: function (data) {
-      var addr = ''; // 주소 변수
+      let addr = ''; // 주소 변수
 
       // 주소 타입 선택 여부
       if (data.userSelectedType === 'R') {
@@ -145,10 +134,12 @@ productData.forEach((v, i) => {
   document.querySelector('.product-info-table').innerHTML += `
     <tr class="trhr"></tr>
     <tr>
-      <td><a href ='#'><img src=${v.img} ${v.productName}/></a></td>
+      <td><a href ='#'><img id="order-product-img"src=${v.productImage} ${
+    v.productName
+  }/></a></td>
       <td>${v.productName}</td>
       <td>${v.amount}개</td>
-      <td>${v.price.toLocaleString()}원</td>
+      <td>${v.productPrice.toLocaleString()}원</td>
     </tr>
     <tr class="trhr"></tr>
   `;
@@ -157,7 +148,7 @@ productData.forEach((v, i) => {
 // 구매 상품 가격 합계
 const getProductSum = () => {
   return productData.reduce((sum, v) => {
-    return sum + v.price * v.amount;
+    return sum + v.productPrice * v.amount;
   }, 0);
 };
 
--- views/product-detail/product-detail.js
+++ views/product-detail/product-detail.js
@@ -7,22 +7,24 @@ console.log(decodeURI(receivedDetailData));
 fetch(`http://127.0.0.1:5555/api/products/${receivedDetailData}`, {
   method: 'GET',
   headers: {
-    'Content-Type': 'application/json;charset=utf-8'
+    'Content-Type': 'application/json;charset=utf-8',
   },
 })
-  .then((res)=> res.json())
-  .then((data)=>{
-    const detailInfo = data.data; 
-    console.log('detailInfo', detailInfo)
+  .then((res) => res.json())
+  .then((data) => {
+    const detailInfo = data.data;
+    console.log('detailInfo', detailInfo);
 
-    document.getElementById("productImg").innerHTML = `
+    document.getElementById('productImg').innerHTML = `
     <img src="http://127.0.0.1:5555${detailInfo.thumbnail.path}" alt="">
-    `
+    `;
 
-    const saledPriceValue = detailInfo.originPrice - (detailInfo.originPrice * (detailInfo.discountRate / 100))
-    const saledPrice = saledPriceValue.toLocaleString()
+    const saledPriceValue =
+      detailInfo.originPrice -
+      detailInfo.originPrice * (detailInfo.discountRate / 100);
+    const saledPrice = saledPriceValue.toLocaleString();
 
-    document.getElementById("productInfo").innerHTML = `
+    document.getElementById('productInfo').innerHTML = `
     <h2>${detailInfo.name}</h2>
     <span>${detailInfo.discountRate}%</span>
     <div class="productPrice">
@@ -54,37 +56,59 @@ fetch(`http://127.0.0.1:5555/api/products/${receivedDetailData}`, {
       <button class="btnCart">장바구니</button>
       <button class="btnBuynow">바로 구매</button>
     </div>
-    `
+    `;
 
-    for(let i = 0; i < detailInfo.descriptions.length; i++) {
-      document.getElementById("desImg").innerHTML += `
+    for (let i = 0; i < detailInfo.descriptions.length; i++) {
+      document.getElementById('desImg').innerHTML += `
       <img src="http://127.0.0.1:5555${detailInfo.descriptions[i].path}" alt="">
-      `
+      `;
     }
 
-    // 상품 수량 버튼 클릭 이벤트 처리 
-    const minusBtn = document.querySelector('.minus')
-    const plusBtn = document.querySelector('.plus')
+    // 상품 수량 버튼 클릭 이벤트 처리
+    const minusBtn = document.querySelector('.minus');
+    const plusBtn = document.querySelector('.plus');
     const cartCountInput = document.querySelector('.cartCount');
     const totalPrice = document.querySelector('.totalPrice');
 
     // minus 버튼 클릭 이벤트 처리
-    minusBtn.addEventListener('click', function() {
+    minusBtn.addEventListener('click', function () {
       let cartCount = parseInt(cartCountInput.value);
       if (cartCount > 1) {
         cartCount--;
       }
-      cartCountInput.value = cartCount; 
+      cartCountInput.value = cartCount;
       let totalPriceValue = saledPriceValue * cartCount;
-      totalPrice.innerHTML = totalPriceValue.toLocaleString();  
+      totalPrice.innerHTML = totalPriceValue.toLocaleString();
     });
-    
+
     // plus 버튼 클릭 이벤트 처리
-    plusBtn.addEventListener('click', function() {
+    plusBtn.addEventListener('click', function () {
       let cartCount = parseInt(cartCountInput.value);
       cartCount++;
       cartCountInput.value = cartCount;
       let totalPriceValue = saledPriceValue * cartCount;
       totalPrice.innerHTML = totalPriceValue.toLocaleString();
     });
-  })
+
+    const buyButton = document.querySelector('.btnBuynow');
+
+    // 구매 이벤트
+    const onBuyProduct = () => {
+      const amount = document.querySelector('.cartCount');
+
+      const product = {
+        productId: receivedDetailData,
+        amount: Number(amount.value),
+        productImage: `http://127.0.0.1:5555${detailInfo.thumbnail.path}`,
+        productName: detailInfo.name,
+        productPrice: detailInfo.originPrice,
+      };
+
+      console.log(product);
+
+      localStorage.setItem('buyProduct', JSON.stringify([product]));
+      window.location.href = `/views/order/order.html`;
+    };
+
+    buyButton.addEventListener('click', onBuyProduct);
+  });
