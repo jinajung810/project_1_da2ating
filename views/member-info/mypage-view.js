449d9ea1db71d19e50a5ad38d03f9635da9d98e0
jinajung810 <jjina810@gmail.com>
2023-07-14 15:18:56 +0900

    feat: update mypage-view

--- views/member-info/mypage-view.js
+++ views/member-info/mypage-view.js
@@ -1,55 +1,28 @@
+initMypageView();
 
-// 마이페이지-회원정보 조회
-// 토큰 가져오기
-const token = sessionStorage.getItem('token');
-
-// 토큰 유효성 검사 함수
-function isValidToken(token) {
-  return token !== null && token !== undefined && token.trim() !== '';
-}
-
-if (isValidToken(token)) {
-  fetchMemberInfo();
-} else {
-  // 로그인되지 않은 상태 처리
-  console.log('로그인되지 않은 상태입니다.');
-}
-// 회원정보 조회 함수
-async function fetchMemberInfo() {
-  try {
-    const response = await fetch('http://127.0.0.1:5555/api/users/my-info', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }
-    });
-    if (response.ok) {
-      const data = await response.json();
-      console.log('회원정보:', data);
-      // 회원정보를 화면에 표시하는 함수 호출
-      memberView(data);
-    } else {
-      // 회원정보 조회 실패 처리
-      console.error('회원정보 조회 실패');
-    }
-  } catch (error) {
-    console.error('회원정보 조회 에러:', error);
+function initMypageView() {
+  const token = sessionStorage.getItem('token');
+  
+  if (token === null) {
+    alert('로그인이 필요합니다.');
+    location.href = '../login/login.html';
+    return;
   }
+
+  memberView();
 }
 
-// 회원정보를 화면에 표시하는 함수
-function memberView(data) {
+// 회원정보 조회 함수
+async function memberView() {
+  const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+  
   const userName = document.querySelector('.userName');
   const email = document.querySelector('.email');
   const phone = document.querySelector('.phone');
   const address = document.querySelector('.address');
 
-  userName.innerText = `${data.name}`;
-  email.innerText = `${data.email}`;
-  phone.innerText = `${data.phone}`;
-  address.innerText = `${data.address}`;
-}
-
-function changeInfo() {
-  location.href = 'http://127.0.0.1:5500/views/member-info/info-change.html';
+  userName.value = userInfo.name;
+  email.value = userInfo.email;
+  phone.value = userInfo.phone || '없음';
+  address.value = userInfo.address || '없음';
 }
