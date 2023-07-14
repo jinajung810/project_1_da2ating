24baccb9cf08d224546596a8b0f56684e51d6544
jinajung810 <jjina810@gmail.com>
2023-07-15 00:35:28 +0900

    feat: update mypage-order-detail

--- views/member-info/order-detail.js
+++ views/member-info/order-detail.js
@@ -45,79 +45,42 @@ async function getOrderDetail(token, orderId) {
 
 function orderView(item) {
   console.log('test', item);
-  
+
   const date = new Date(item.createdAt);
-  
+
   document.querySelector('#payment-price').textContent = (item.totalProductPrice + item.deliveryPrice).toLocaleString() + '원';
   document.querySelector('#order-date').textContent = date.toLocaleString();
   document.querySelector('#order-status').textContent = getStatusKorText(item.status);
   if (item.status === 'order-receipt') {
-    document.querySelector('.info-right-box').innerHTML = `
-      <button class="">주문 취소</button>
+    document.querySelector('.cancel-btn-box').innerHTML = `
+      <button class="action-btn" data-id="${item._id}" onclick="confirmCancelOrder(this)">주문 취소</button>
     `;
   }
-  
-  
-  
-  
-//  {
-//    "_id": "64b15bc6cf692c216ec88574",
-//    "orderUser": "64b1277a5e67af884c3ebd80",
-//    "totalProductPrice": 19400,
-//    "deliveryPrice": 3000,
-//    "status": "order-receipt",
-//    "deliveryMessage": null,
-//    "receiverName": "최원진",
-//    "receiverPhone": "010-1234-1234",
-//    "receiverZipCode": "06541",
-//    "receiverAddress": "서울 서초구 강남대로 477",
-//    "receiverDetailAddress": "123123123",
-//    "productCount": 2,
-//    "repProductName": "야채 볶음밥&햄에그롤",
-//    "repProductImage": "/public/09c67fb9-fa17-4912-8337-6926acb117fd-283_detail_01.jpg",
-//    "createdAt": "2023-07-14T14:29:26.929Z",
-//    "updatedAt": "2023-07-14T14:29:26.929Z",
-//    "__v": 0,
-//    "orderDetails": [
-//        {
-//            "_id": "64b15bc6cf692c216ec88576",
-//            "orderId": "64b15bc6cf692c216ec88574",
-//            "product": {
-//                "_id": "64b15adccf692c216ec884b3",
-//                "name": "야채 볶음밥&햄에그롤",
-//                "thumbnail": {
-//                    "_id": "64b15adccf692c216ec884af",
-//                    "path": "/public/09c67fb9-fa17-4912-8337-6926acb117fd-283_detail_01.jpg"
-//                }
-//            },
-//            "amount": 1,
-//            "productPrice": 4700,
-//            "productDiscountRate": null,
-//            "__v": 0,
-//            "createdAt": "2023-07-14T14:29:26.939Z",
-//            "updatedAt": "2023-07-14T14:29:26.939Z"
-//        },
-//        {
-//            "_id": "64b15bc6cf692c216ec88577",
-//            "orderId": "64b15bc6cf692c216ec88574",
-//            "product": {
-//                "_id": "64b157cdcf692c216ec8829a",
-//                "name": "취나물밥&매콤 제육볶음",
-//                "thumbnail": {
-//                    "_id": "64b157cdcf692c216ec88296",
-//                    "path": "/public/8b63efaf-ddd0-4ee2-a8df-9323c6bb9670-263_detail_016.jpg"
-//                }
-//            },
-//            "amount": 3,
-//            "productPrice": 4900,
-//            "productDiscountRate": null,
-//            "__v": 0,
-//            "createdAt": "2023-07-14T14:29:26.939Z",
-//            "updatedAt": "2023-07-14T14:29:26.939Z"
-//        }
-//    ]
-//}
 
+  document.querySelector('#receiver-name').textContent = item.receiverName;
+  document.querySelector('#receiver-phone').textContent = item.receiverPhone;
+  document.querySelector('#receiver-addr').textContent = `(${item.receiverZipCode}) ${item.receiverAddress} ${item.receiverDetailAddress}`;
+  document.querySelector('#receiver-msg').textContent = item.deliveryMessage || '-';
+
+  let productTags = '';
+  item.orderDetails.forEach(detail => {
+    productTags += `
+      <div class="order-product-item">
+        <div class="img-box">
+          <img
+            src="http://kdt-sw-5-team02.elicecoding.com${detail.product.thumbnail.path}"
+          />
+        </div>
+        <div class="product-info-panel">
+          <div class="product-name">${detail.product.name}</div>
+          <div class="product-desc">가격. ${detail.productPrice.toLocaleString()}원</div>
+          <div class="product-desc">수량. ${detail.amount}개</div>
+          <div class="product-desc">총 가격. ${(detail.amount * detail.productPrice).toLocaleString()}원</div>
+        </div>
+      </div>
+    `;
+  });
+  document.querySelector('.order-product-list').innerHTML = productTags;
 }
 
 function getStatusKorText(status) {
@@ -146,81 +109,29 @@ function getStatusKorText(status) {
   }
 }
 
+function confirmCancelOrder(elm) {
+  if (confirm('정말 취소하시겠습니까?')) {
+    const orderId = elm.getAttribute('data-id');
+    cancelOrder(orderId);
+  }
+}
 
-//달력에서 시작날짜와 종료날짜 지정
-//function getSelectedDateRange() {
-//  const startDate = document.querySelector('input[name="startDate"]').value;
-//  const endDate = document.querySelector('input[name="endDate"]').value;
-  
-//  return { startDate, endDate };
-//}
-
-//주문검색 버튼 클릭이벤트 리스너
-//function handleSearchButtonClick(event) {
-//    event.preventDefault();
-//    performSearch();
-//}
-  //startDate, endDate, orders 변수 지정=>지정한 날짜에 맞는 orders데이터 출력
-//async function performSearch() {
-//  const { startDate, endDate } = getSelectedDateRange();
-//  const orders = await fetchOrders();
-//  //!오류발생)order-deliver.js:48 Uncaught (in promise) ReferenceError: fetchOrders is not defined at performSearch
-
-//  const filteredOrders = orders.filter(order => {
-//    const orderDate = new Date(order.createdAt.substring(0, 10));
-//    return orderDate >= new Date(startDate) && orderDate <= new Date(endDate);
-//  });
-//  orderView(filteredOrders);
-//}
-
-////기간 버튼 별 클릭이벤트
-//const periodButtons = document.querySelectorAll('.period input[type="button"]');
-//periodButtons.forEach(button => {
-//    button.addEventListener('click', function (e) {
-//      e.preventDefault()
-//      const calculateDateRange = (selectedPeriod) => {
-//        const currentDate = new Date();
-//        let startDate, endDate ;
-      
-//        if (selectedPeriod === '오늘') {
-//          startDate = formatDate(currentDate);
-//          endDate = formatDate(currentDate);
-//        } else if (selectedPeriod === '7일') {
-//          startDate = formatDate(new Date(currentDate.getTime() - 7 * 24 * 60 * 60 * 1000));
-//          endDate = formatDate(currentDate);
-//        } else if (selectedPeriod === '15일') {
-//          startDate = formatDate(new Date(currentDate.getTime() - 15 * 24 * 60 * 60 * 1000));
-//          endDate = formatDate(currentDate);
-//        } else if (selectedPeriod === '1개월') {
-//          startDate = formatDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, currentDate.getDate()));
-//          endDate = formatDate(currentDate);
-//        } else if (selectedPeriod === '3개월') {
-//          startDate = formatDate(new Date(currentDate.getFullYear(), currentDate.getMonth() - 3, currentDate.getDate()));
-//          endDate = formatDate(currentDate);
-//        } else if (selectedPeriod === '1년') {
-//          startDate = formatDate(new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate()));
-//          endDate = formatDate(currentDate);
-//        }
-      
-//        return { startDate, endDate };
-//      };
-      
-//      const formatDate = (date) => {
-//        const year = date.getFullYear();
-//        const month = String(date.getMonth() + 1).padStart(2, '0');
-//        const day = String(date.getDate()).padStart(2, '0');
-//        return `${year}-${month}-${day}`;
-//      };
-
-//      const selectedPeriod = e.target.value;
-//      const dateRange = calculateDateRange(selectedPeriod);
-//      const startDateInput = document.querySelector('input[name="startDate"]');
-//      const endDateInput = document.querySelector('input[name="endDate"]');
-//      startDateInput.value = dateRange.startDate;
-//      endDateInput.value = dateRange.endDate;
-//    });
-    
-//  });
-  
-  //const searchButton = document.querySelector('.period button[type="submit"]');
-  //searchButton.addEventListener('click', handleSearchButtonClick);
+async function cancelOrder(orderId) {
+  const token = sessionStorage.getItem('token');
+  try {
+    const response = await fetch(`http://kdt-sw-5-team02.elicecoding.com/api/users/my-orders/${orderId}/cancel`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    });
+    if (response.ok) {
+      location.reload();
+    } else {
+      console.error('주문 접수 상태에서만 취소할 수 있습니다.');
+    }
+  } catch (error) {
+    console.error('주문정보 조회 에러:', error);
+  }
+}
